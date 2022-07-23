4fd8f1f641c26a37f163116bacfa327493699733
njtsb1 <njtsb1@gmail.com>
2022-07-22 22:48:03 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+Daily learning
+
+# Cloud Parking
+
+# Performing a Cloud Deployment of a set of APIs developed in Spring Boot
+
+Project developed at Digital Innovation One's Bootcamp Spring Framework Experience with expert guidance [Sandro Giacomozzi](https://github.com/sandrogiacom "Sandro Giacomozzi"). 
+Learning to develop a set of APIs using Spring Boot to control a parking lot. Controls entry, exit and amount to be charged from the customer. Application of all API development best practices, including security with Spring Security and access to the PostgreSQL database. Conducting tests and test coverage reports. Execution of the Heroku cloud deployment to make the API available on the Internet.
+
+## Run database
+docker run --name parking-db -p 5432:5432 -e POSTGRES_DB=parking -e POSTGRES_USER=admin -e POSTGRES_PASSWORD=123 -d postgres:10-alpine
+
+## Start and Stop
+
+### Stop Database
+docker stop parking-db
+
+### Start Database
+docker start parking-db
+
+[LICENSE](./LICENSE)
+# CLOUD_PARKING
---
+++ src/test/java/one/digitalinnovation/parking/controller/ParkingControllerTest.java
@@ -0,0 +1,54 @@
+package one.digitalinnovation.parking.controller;
+
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+import io.restassured.RestAssured;
+import one.digitalinnovation.parking.controller.dto.ParkingCreateDTO;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class ParkingControllerTest extends AbstractContainerBase {
+
+    @LocalServerPort
+    private int randomPort;
+
+    @BeforeEach
+    public void setUpTest() {
+        RestAssured.port = randomPort;
+    }
+
+    @Test
+    void whenFindAllThenCheckResult() {
+        RestAssured.given()
+                .auth().basic("user", "12345")
+                .when()
+                .get("/parking")
+                .then()
+                .statusCode(HttpStatus.OK.value());
+    }
+
+    @Test
+    void whenCreateThenCheckIsCreated() {
+        var createDTO = new ParkingCreateDTO();
+        createDTO.setColor("YELLOW");
+        createDTO.setLicense("DAF-2022");
+        createDTO.setModel("MBENZ");
+        createDTO.setState("SP");
+
+        RestAssured.given()
+                .when()
+                .auth().basic("user", "12345")
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(createDTO)
+                .post("/parking")
+                .then()
+                .statusCode(HttpStatus.CREATED.value())
+                .body("license", Matchers.equalTo("DAF-5555"))
+                .body("color", Matchers.equalTo("YELLOW"));
+    }
+}
